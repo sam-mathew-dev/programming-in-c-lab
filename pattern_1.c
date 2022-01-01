dd3123939ecf1c4840f4746c3d3229ffb55c3394
Sammathew9961 <96916212+Sammathew9961@users.noreply.github.com>
2022-01-01 18:38:23 +0530

    Create pattern_1.c

---
+++ pattern_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+    int i, j;
+    clrscr();
+    for (i = 1; i <= 5; i++) {
+        for (j = 1; j <= 5; j++) {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+    getch();
+}
